bc6e333804e526e8705f1c1b04b5308537f94a61
Thamires <thamiresdepontesdasilva@gmail.com>
2022-04-12 15:02:48 -0300

    Teste de Encoder

--- Codigos CompInterna2022/src/main.cpp
+++ Codigos CompInterna2022/src/main.cpp
@@ -2,6 +2,8 @@
 #include <MotorDC.h>
 #include <SensorColor.h>
 #include <Ultrasonic.h>
+#include <Move.h>
+
 
 // Motor Direita
 #define pin1A 5
@@ -35,8 +37,8 @@
 #define pinColorLeftOut 43
 
 
-MotorDC MotorRight(pin1A, pin1B, pin1pwm, pin1Enc);
-MotorDC MotorLeft(pin2A, pin2B, pin2pwm, pin2Enc);
+MotorDC motorRight (5, 7, 8, 12); 
+MotorDC motorLeft (6, 4, 9, 13);
 
 Ultrasonic SensorUltraFront(pinFrontTrigger, pinFrontEcho);
 Ultrasonic SensorUltraLeft(pinLeftTrigger, pinLeftEcho);
@@ -45,61 +47,44 @@ Ultrasonic SensorUltraRight(pinRightTrigger, pinRightEcho);
 SensorColor SensorColorLeft(pinColorLeftS0, pinColorLeftS1, pinColorLeftS2, pinColorLeftS3, pinColorLeftOut);
 
 
+//Declaração de variáveis que serão parâmetros na função moveAllpid.
+float soma = 0;
+float error [2];
+float giro = 0;
+float mR;
+float mL;
+
+void inc (){
+
+  motorLeft.encSignal();
+  
+}
+
+void incR (){
+
+	motorRight.encSignal();
+}
+
 void setup() {
   Serial.begin(9600);
-
+  attachInterrupt(digitalPinToInterrupt(13), inc, RISING);
+	attachInterrupt(digitalPinToInterrupt(12), incR, RISING);
+  error[0] = 0;
+	error[1] = millis();
 }
 
 
 void loop() {
-  // MotorRight.fwd(70);
-  // MotorLeft.fwd(70);
-  // delay(5000);
-  // MotorRight.stop();
-  // MotorLeft.stop();
-  // delay(5000);
-  // MotorRight.rev(70);
-  // MotorLeft.rev(70);
-  // delay(5000);
 
-  //Le as informacoes do sensor, em cm e pol
-  // float cmMsecFront, cmMsecLeft, cmMsecRight;
-  // long microsecFront = SensorUltraFront.timing();
-  // long microsecLeft = SensorUltraLeft.timing();
-  // long microsecRight = SensorUltraRight.timing();
-  // cmMsecFront = SensorUltraFront.convert(microsecFront, Ultrasonic::CM);
-  // cmMsecLeft = SensorUltraLeft.convert(microsecLeft, Ultrasonic::CM);
-  // cmMsecRight = SensorUltraRight.convert(microsecRight, Ultrasonic::CM);
-  // //Exibe informacoes no serial monitor
-  // Serial.print("Distancia frente em cm: ");
-  // Serial.print(cmMsecFront);
-  // Serial.print("\n");
-  // Serial.print("Distancia esquerda em cm: ");
-  // Serial.print(cmMsecLeft);
-  // Serial.print("\n");
-  // Serial.print("Distancia direita em cm: ");
-  // Serial.print(cmMsecRight);
-  // Serial.print("\n");
-  // Serial.print("\n");
-  // delay(1000);
+  mL = motorLeft.getCount();
+	mR = motorRight.getCount();
+	giro = mL - mR;
+	soma = 0;
 
-  SensorColorLeft.readColors(); // Chama a rotina que le as cores
+  Serial.print("mL: ");
+	Serial.print(mL);
+	Serial.print("mR: ");
+	Serial.print(mR);
 
-  // Verifica se a cor vermelha foi detectada
-  if (SensorColorLeft.vermelho < SensorColorLeft.azul && SensorColorLeft.vermelho < SensorColorLeft.verde){
-    Serial.print("VERMELHO");
-    Serial.print("\n");
-  }
-  
-  if (SensorColorLeft.azul < SensorColorLeft.vermelho && SensorColorLeft.azul < SensorColorLeft.verde) { // Verifica se a cor azul foi detectada
-    Serial.print("AZUL");
-    Serial.print("\n");
-  }
-  
-  if (SensorColorLeft.verde < SensorColorLeft.vermelho && SensorColorLeft.verde < SensorColorLeft.azul) { // Verifica se a cor verde foi detectada
-    Serial.print("VERDE");
-    Serial.print("\n");
-  }
-
-  delay(1000); // Aguarda 1000 milissegundos
+  moveAllpid(50, &motorLeft, &motorRight, &soma, error, giro);
 }
